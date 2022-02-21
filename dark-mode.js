45bd36b4ea22b1eb8152bdc61a42d23ebcef87ca
Laura Abend <laura.l.abend@gmail.com>
2022-02-21 16:26:39 -0600

    initial commit

---
+++ dark-mode.js
@@ -0,0 +1,17 @@
+$(function () {
+    let darkModeOn = false;
+    const darkModeString = "Dark mode is";
+    const onString = "on";
+    const offString = "off";
+    $("input").on("click", function() {
+      if (!darkModeOn) {
+        darkModeOn = true;
+        $(".dark-mode-status").text(darkModeString + " " + onString);
+        $("body, a").addClass("dark-mode-on");
+      } else {
+        darkModeOn = false;
+        $(".dark-mode-status").text(darkModeString + " " + offString);
+        $("body, a").removeClass("dark-mode-on");
+      }
+    });
+  });  
