bd97e06c2227c4556020248267bbf140b19d9549
Laura Abend <laura.l.abend@gmail.com>
2022-02-26 08:58:09 -0600

    add data attributes for cypress testing

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -4,31 +4,36 @@ describe('The Home Page', () => {
     })
 
     it('displays a header', () => {
-      cy.get('h1').contains('I am a developer.')
+      cy.get('[data-cy=main-header]').contains('I am a developer.')
     })
 
     it('displays the current year', () => {
-      cy.get('.year').contains(new Date().getFullYear())
+      cy.get('[data-cy=current-year]').contains(new Date().getFullYear())
     })
 
     describe('dark mode toggle', () => {
+      const dmOff = 'Dark mode is off';
+      const dmOn = 'Dark mode is on';
+      const lightStyling = 'rgb(255, 255, 255)';
+      const darkStyling = 'rgb(51, 51, 51)';
+
       it('updates the background & font color', () => {
-        cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
-        cy.get('body').should('have.css', 'color', 'rgb(51, 51, 51)')
-        cy.get('.slider').click()
-        cy.get('body').should('have.css', 'background-color', 'rgb(51, 51, 51)')
-        cy.get('body').should('have.css', 'color', 'rgb(255, 255, 255)')
-        cy.get('.slider').click()
+        cy.get('[data-cy=all-content]').should('have.css', 'background-color', lightStyling)
+        cy.get('[data-cy=all-content]').should('have.css', 'color', darkStyling)
+        cy.get('[data-cy=dark-mode-toggle]').click()
+        cy.get('[data-cy=all-content]').should('have.css', 'background-color', darkStyling)
+        cy.get('[data-cy=all-content]').should('have.css', 'color', lightStyling)
+        cy.get('[data-cy=dark-mode-toggle]').click()
       })
       
       it('updates dark mode text', () => {
-        cy.get('.dark-mode-status').contains('Dark mode is off')
-        cy.get('.slider').click()
-        cy.get('.dark-mode-status').contains('Dark mode is on')
-        cy.get('body').should('have.css', 'background-color', 'rgb(51, 51, 51)')
-        cy.get('.slider').click()
-        cy.get('.dark-mode-status').contains('Dark mode is off')
-        cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
+        cy.get('[data-cy=dark-mode-text]').contains(dmOff)
+        cy.get('[data-cy=dark-mode-toggle]').click()
+        cy.get('[data-cy=dark-mode-text]').contains(dmOn)
+        cy.get('[data-cy=all-content]').should('have.css', 'background-color', darkStyling)
+        cy.get('[data-cy=dark-mode-toggle]').click()
+        cy.get('[data-cy=dark-mode-text]').contains(dmOff)
+        cy.get('[data-cy=all-content]').should('have.css', 'background-color', lightStyling)
       })
     })
   })
